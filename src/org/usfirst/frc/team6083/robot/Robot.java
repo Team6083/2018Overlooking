25691dfac7bd37baa9d64668f6a7624532931186
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-01-30 19:40:39 +0800

    Simple Teleop V1

--- src/org/usfirst/frc/team6083/robot/Robot.java
+++ src/org/usfirst/frc/team6083/robot/Robot.java
@@ -8,44 +8,39 @@
 package org.usfirst.frc.team6083.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends IterativeRobot {
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	Joystick stick = new Joystick(0);
+	VictorSP Lmotor1, Lmotor2, Rmotor1, Rmotor2, UPmotor, Smotor1, Smotor2;
+
+	private final static double error_range = 0.1;
+	private final static double Sspeed = 0.7;
+	private static double speedl, speedr, LeftY, RightY;
+	
+
 	@Override
 	public void robotInit() {
 		m_chooser.addDefault("Default Auto", kDefaultAuto);
 		m_chooser.addObject("My Auto", kCustomAuto);
 		SmartDashboard.putData("Auto choices", m_chooser);
+		Lmotor1 = new VictorSP(0);
+		Lmotor2 = new VictorSP(1);
+		Rmotor1 = new VictorSP(2);
+		Rmotor2 = new VictorSP(3);
+		UPmotor = new VictorSP(4);
+		Smotor1 = new VictorSP(5);
+		Smotor2 = new VictorSP(6);
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	@Override
 	public void autonomousInit() {
 		m_autoSelected = m_chooser.getSelected();
@@ -54,33 +49,79 @@ public class Robot extends IterativeRobot {
 		System.out.println("Auto selected: " + m_autoSelected);
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
 		switch (m_autoSelected) {
-			case kCustomAuto:
-				// Put custom auto code here
-				break;
-			case kDefaultAuto:
-			default:
-				// Put default auto code here
-				break;
+		case kCustomAuto:
+			// Put custom auto code here
+			break;
+		case kDefaultAuto:
+		default:
+			// Put default auto code here
+			break;
 		}
 	}
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
 	@Override
 	public void teleopPeriodic() {
+		
+		// Remove error value
+		if (stick.getRawAxis(1) <= -error_range || stick.getRawAxis(1) > error_range) { // Yaxis (left)
+			LeftY = -stick.getRawAxis(1);
+		} else {
+			LeftY = 0;
+		}
+
+		if (stick.getRawAxis(5) <= -error_range || stick.getRawAxis(5) > error_range) { // Yaxis (right)
+			RightY = -stick.getRawAxis(5);
+		} else {
+			RightY = 0;
+		}
+		speedl = LeftY / 3;
+		speedr = RightY / 3;
+		
+		// Speed up when button pressed
+		if (stick.getRawButton(5)) {
+			speedl = speedl * 2;
+		}
+		if (stick.getRawButton(6)) {
+			speedr = speedr * 2;
+		}
+		
+		
+		Lmotor1.set(speedl);
+		Lmotor2.set(speedl);
+		Rmotor1.set(-speedr);
+		Rmotor2.set(-speedr);
+		
+		
+		if (stick.getRawAxis(2) > 0.1) {
+			UPmotor.set(stick.getRawAxis(2));
+		} else if (stick.getRawAxis(3) > 0.1) {
+			UPmotor.set(-stick.getRawAxis(3));
+		} else {
+			UPmotor.set(0);
+		}
+
+		if (stick.getRawButton(9)) {
+			Smotor1.set(Sspeed);
+			Smotor2.set(-Sspeed);
+		} else if (stick.getRawButton(10)) {
+			Smotor1.set(-Sspeed);
+			Smotor2.set(Sspeed);
+		} else {
+			Smotor1.set(0);
+			Smotor2.set(0);
+		}
+
+		SmartDashboard.putNumber("leftmotor", Lmotor1.get());
+		SmartDashboard.putNumber("rightmotor", Rmotor1.get());
+		SmartDashboard.putNumber("UPmotor", UPmotor.get());
+		SmartDashboard.putNumber("Smotor", Smotor1.get());
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
 	@Override
 	public void testPeriodic() {
+
 	}
 }
