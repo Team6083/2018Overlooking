5c7f356c10e2c49e695870ac7d1755ee64339386
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-03-11 06:09:55 +1100

    Fix baseline

--- src/System/Autonomous/modes/Baseline.java
+++ src/System/Autonomous/modes/Baseline.java
@@ -4,43 +4,38 @@ import System.UpAssembly;
 import System.Autonomous.AutoEngine;
 
 public class Baseline extends AutoEngine {
-	
+
 	private static final double baseLineDis = -120;// In inch
 
 	public static void loop() {
 		switch (step) {
-		/*case 0:
-			currentStep = "Set Raise Up";
-			 UpAssembly.moveStep(1);
-			nextStep();
-			break;
-		case 1:
-			currentStep = "Raise Up";
-			if (UpAssembly.isReachTarget())nextStep();
-			leftSpeed = 0;
-			rightSpeed = 0;
-			break;*/
+		/*
+		 * case 0: currentStep = "Set Raise Up"; UpAssembly.moveStep(1); nextStep();
+		 * break; case 1: currentStep = "Raise Up"; if
+		 * (UpAssembly.isReachTarget())nextStep(); leftSpeed = 0; rightSpeed = 0; break;
+		 */
 		case 0:
 			currentStep = "Go forward";
-			if(station!=2) {
+			if (station != 2) {
 				walk(baseLineDis);
-				step=-1;
-			}else {
-				gyrowalker.setTargetAngle(switchPos==1?-155:155);
+				step = -1;
+			} else {
+				gyrowalker.setTargetAngle(switchPos == 1 ? -155 : 155);
+				walk(-140);
 			}
-			nextStep();
 			break;
-			case 1:
+		case 1:
 			currentStep = "Turn1";
 			leftSpeed = 0;
 			rightSpeed = 0;
-			if(gyrowalker.getErrorAngle() < 10) {
+			if (gyrowalker.getErrorAngle() < 10) {
 				nextStep();
 			}
 			break;
-			case 2:
-				currentStep = "walk";
-				walk(135);
+		case 2:
+			currentStep = "walk";
+			walk(135);
+			break;
 		default:
 			currentStep = "none";
 			leftSpeed = 0;
