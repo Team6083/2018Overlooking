2b5407b40ff60aece0947f2b30154779214d3b3e
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-08-25 11:01:53 +0800

    lower climb speed

--- src/System/ClimbAssembly.java
+++ src/System/ClimbAssembly.java
@@ -30,7 +30,7 @@ public class ClimbAssembly {
 			stringtalon.set(ControlMode.PercentOutput, 1);
 		}
 		else if(Joysticks.pov == 90) {
-			climbmotor.set(-0.6);
+			climbmotor.set(-0.5);
 		}
 		else if(Joysticks.pov == 270) {
 			climbmotor.set(0.4);
